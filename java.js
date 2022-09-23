f75472f455c68eeaf3a0d0a96a24105e2e29c3f1
marq08s <113584560+marq08s@users.noreply.github.com>
2022-09-23 11:13:41 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,15 @@
+function multiplicapor(){
+    
+let num1=document.getElementById("numeroum").value;
+let num2=document.getElementById("numerodois").value;
+document.getElementById("resultado").innerHTML = num1 *num2;
+
+}
+
+function dividepor(){
+
+let num1=document.getElementById("numeroum").value;
+let num2=document.getElementById("numerodois").value;
+document.getElementById("resultado").innerHTML = num1 / num2;
+
+}
